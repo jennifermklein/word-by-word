5af510488b97a661576da59ab710c9772458505b
Jenny Klein <97071629+jennifermklein@users.noreply.github.com>
2022-02-24 21:05:57 -0500

    update error message

--- static/wordbyword.js
+++ static/wordbyword.js
@@ -1,8 +1,9 @@
 document.addEventListener('DOMContentLoaded',function() {
     const story = document.querySelector('#curr_story');
     const addWord = document.querySelector('#add_word');
-    const errorMessage = document.querySelector('#apology');
-    const endStory = document.querySelector('#end_story');
+    // const errorMessage = document.querySelector('#apology');
+    const errorMessage2 = document.querySelector('#apology2');
+    // const endStory = document.querySelector('#end_story');
     
     function updateStory () {
         axios.get('/story')
@@ -11,23 +12,29 @@ document.addEventListener('DOMContentLoaded',function() {
                     story.innerHTML = response.data;
                 }
                 // if (!response.data) {
-                //     document.querySelector('#end_story').style.display = 'none';
+                //     document.querySelector('#end_story').style.visibility = 'hidden';
                 // }
                 // else {
-                //     document.querySelector('#end_story').style.display = 'inline';
+                //     document.querySelector('#end_story').style.visibility = 'visibile';
                 // }
             })
     }
     
     setInterval(updateStory, 500);
 
+    function resetErrorMessage(){
+        errorMessage2.style.color = "rgb(115, 115, 115)";
+        errorMessage2.innerHTML = "Type a single word and press enter to submit";
+    }
+
     // show error if submits word and then keeps typing
     addWord.addEventListener('keypress',function(event){
         axios.get('/session_error')
             .then(function(response) {
                 if (response.data === 'True') {
-                    errorMessage.style.display = 'inline';
-                    errorMessage.innerHTML = "It's someone else's turn to add a word";
+                    errorMessage2.style.color = "#D8000C";
+                    errorMessage2.innerHTML = "It's someone else's turn to add a word!";
+                    setInterval(resetErrorMessage, 10000);
                 }
             })
             .catch(function (error) {
@@ -49,18 +56,5 @@ document.addEventListener('DOMContentLoaded',function() {
         // }
     })
     
-    // endStory.addEventListener('click',function(event){
-    //     axios.get('/session_error')
-    //         .then(function(response) {
-    //             if (response.data === 'True') {
-    //                 errorMessage.style.display = 'inline';
-    //                 errorMessage.innerHTML = "It's someone else's turn to add a word";
-    //             }
-    //         })
-    //         .catch(function (error) {
-    //             // handle error
-    //             console.log(error);
-    //         })
-    // })
 });
 
